f3cdebc3049fd4ee340272b0a1b065a9984d9819
Adelia <adeliacristinecs@gmail.com>
2020-04-22 13:05:47 -0300

    Primeira pagina html, menu horizontal da segunda pg html e alterações no css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+
+//function getPokemon() {
+   // return POKEMON["pokemon"];
+ // }
+
+document.getElementById("go").addEventListener("click",go);
+
+function go{
+    window.location("personagens.html", "_blank");
+   }
 
 console.log(example, data);
