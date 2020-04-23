7faded8cc4cae5abb1bb3be1b0f4dea7ca792ca9
coderanac <coder.anac@gmail.com>
2020-04-22 23:12:13 -0300

    :poop: starts card creation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let cards = document.querySelector('#cards');
+
+function createTemplateString(keyCharacter) {
+  const datum = data.results[keyCharacter];
+  let constructorTemplateString =  `
+    <section class="cards-box">
+      <img src="${datum.image}"/>
+      <p>${datum.name}</p>
+      <p>${datum.species}</p>
+      <p>${datum.status}</p>
+      <p>${datum.gender}</p>
+      <p>${datum.type}</p>
+      <p>${datum.origin.name}</p>
+      <p>${datum.location.name}</p>
+      <p>${datum.episode.length}</p>
+    </section>
+  `;
+  return constructorTemplateString;
+}
+
+function sendKey() {
+  let html = '';
+  for(let i = 0; i < data.results.length; i++){
+    html += createTemplateString(i)
+  }
+  cards.innerHTML = html;
+}
+
+sendKey();
