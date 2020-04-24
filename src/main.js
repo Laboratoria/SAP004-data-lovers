5435b96d82a42db49f9d51e456518746a898a594
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-24 05:04:14 -0300

    Layout atualizado

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
 import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 const root = document.getElementById("root") // import div
-
 const pokemons = data.pokemon // pokemons = array
 
 
-//  const searchName = p => p.name
-// const nomesPokemons = pokemons.map(searchName)
-
 const print = pokemons => { //função para imprimir os pokemons
-    const div = document.createElement("div") // cria uma nova div
-    div.classList.add("card") // coloca uma clas na div
+    const card = document.createElement("div") // cria uma nova div
     const img = document.createElement("img"); //criar elemento img
+    const cardInformation = document.createElement("div") //cria div das informações
+    
     img.src = pokemons.img // coloca o endereço da imagem
+    
+    card.classList.add("card") // coloca uma clas na div
     img.classList.add("picture") // coloca uma classe na imagem
-    div.appendChild(img) //coloca a imagem na nova div
-    div.innerHTML += `<br><h5>${pokemons.num}</h5>` //coloca o numero
-    div.innerHTML += `<h4>${pokemons.name}</h4>` // coloca o nome
-    div.innerHTML +=  `<h5>${pokemons.type}</h5>` // coloca o tipo
-    root.appendChild(div) // coloca nova div dentro da div existente
+    cardInformation.classList.add("card-information")  
+    cardInformation.innerHTML += `<br><h5>${pokemons.num}</h5>` //coloca o numero
+    cardInformation.innerHTML += `<h4>${pokemons.name}</h4>` // coloca o nome
+    cardInformation.innerHTML +=  `<h5>${pokemons.type}</h5>` // coloca o tipo
+    card.appendChild(img) //coloca a imagem na nova div
+    card.appendChild(cardInformation) // coloca informações no card
+    root.appendChild(card) // coloca nova div dentro da div existente
 }
 pokemons.map(print) // percorre o array e execulta a função print
 
