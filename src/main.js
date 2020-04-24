c629083d96d9bc9ff9e9fc1d7915d4c919fa0523
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-24 02:47:36 -0300

    Estrutura básica do html, estilização básica do css e impressão dos pokémons na tela.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+const root = document.getElementById("root") // import div
 
-console.log(example, data);
+const pokemons = data.pokemon // pokemons = array
+
+
+//  const searchName = p => p.name
+// const nomesPokemons = pokemons.map(searchName)
+
+const print = pokemons => { //função para imprimir os pokemons
+    const div = document.createElement("div") // cria uma nova div
+    div.classList.add("card") // coloca uma clas na div
+    const img = document.createElement("img"); //criar elemento img
+    img.src = pokemons.img // coloca o endereço da imagem
+    img.classList.add("picture") // coloca uma classe na imagem
+    div.appendChild(img) //coloca a imagem na nova div
+    div.innerHTML += `<br><h5>${pokemons.num}</h5>` //coloca o numero
+    div.innerHTML += `<h4>${pokemons.name}</h4>` // coloca o nome
+    div.innerHTML +=  `<h5>${pokemons.type}</h5>` // coloca o tipo
+    root.appendChild(div) // coloca nova div dentro da div existente
+}
+pokemons.map(print) // percorre o array e execulta a função print
+
