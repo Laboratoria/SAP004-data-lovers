5e5a3ae1ca0098811bf61df48ca068351a59b32d
Carol Moscheta <carolina.moscheta@gmail.com>
2020-04-27 17:32:56 -0300

    estruturação do HTML e CSS, - Lógica no main.js, - Imagem de logotpo, - Arquivo reset.css para limpar as pré disposições dos navegadores

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-import { example } from './data.js';
+import {
+  example
+} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+
+let dado = document.querySelector("#lista-pokemon");
+dado.innerHTML = "";
+
+for (let i = 0; i < data.pokemon.length; i++) {
+
+  let dadoImagem = data.pokemon[i].img;
+  let dadoNome = data.pokemon[i].name;
+  let dadoTipo = data.pokemon[i].type
+
+  let li = document.createElement("li")
+  li.classList.add("lista-pokedex-link");
+  li.textContent += dadoNome;
+  dado.appendChild(li);
+
+  let img = document.createElement("img");
+  img.classList.add("lista-img");
+  img.src += dadoImagem;
+  li.appendChild(img);
+
+  let tipo = document.createElement("tipo")
+  tipo.classList.add("lista-tipo")
+  tipo.textContent += dadoTipo
+  li.appendChild(tipo)
+}
 
 console.log(example, data);
