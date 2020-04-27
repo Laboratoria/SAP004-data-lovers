20143629c5119f32ba27b4eb6f23b5f1604676a4
larissamiyaji <natsumi-23@hotmail.com>
2020-04-27 01:32:24 -0300

    Ativação dos botões

--- src/main.js
+++ src/main.js
@@ -5,33 +5,34 @@ import data from './data/lol/lol.js';
 
 console.log(example, data);
 
-const aboutTheGameButton = document.getElementsByClassName('about-the-game');
-const charactersButton = document.getElementsByClassName('characters');
-const youtubeButton = document.getElementsByClassName('youtube');
-const compareButton = document.getElementsByClassName('compare');
+const aboutTheGameButton = document.getElementsByClassName('about-the-game-button');
+const charactersButton = document.getElementsByClassName('characters-button');
+const youtubeButton = document.getElementsByClassName('youtube-button');
+const compareButton = document.getElementsByClassName('compare-button');
 const closePopupButton = document.getElementsByClassName('close-popup-button');
 
 function aboutTheGame() {
-    openPopup() /*ABRE O POP UP E MOSTRA INFORMAÇÕES SOBRE O JOGO*/
-    closePopup()/*FECHA O POP UP*/
+    location.href="about-the-game.html";
+    /*openPopup() ABRE O POP UP E MOSTRA INFORMAÇÕES SOBRE O JOGO*/
+    /*closePopup() FECHA O POP UP*/
 }
 aboutTheGameButton.addEvendListener('click', aboutTheGame);
 closePopupButton.addEvendListener('click', aboutTheGame);
 
-function openPopup(){
+/*function openPopup(){
     //FUNÇÃO DE ABRIR POPUP
 }
 function closePopup(){
     //FUNÇÃO DE FECHAR POPUP
-}
+}*/
 //------------------------------------------------------------------------
 function showCharacters() {
-    /*MOSTRAR INFORMAÇÕES SOBRE OS PERSONAGENS*/
+    location.href="characters.html"; /*MOSTRAR INFORMAÇÕES SOBRE OS PERSONAGENS*/
 }
 charactersButton.addEvendListener('click', showCharacters);
 
 function showYoutube() {
-    /*MOSTRAR INFORMAÇÕES SOBRE YOUTUBE*/
+    location.href="youtube.html"; /*MOSTRAR INFORMAÇÕES SOBRE YOUTUBE*/
 }
 youtubeButton.addEvendListener('click', showYoutube);
 
