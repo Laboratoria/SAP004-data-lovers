6c626d6ab05f02de550537b6ad5e417f9718eba0
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-28 05:10:13 -0300

    Poderes com cores na descrição

--- src/main.js
+++ src/main.js
@@ -8,17 +8,31 @@ const print = pokemons => { //função para imprimir os pokemons
     const card = document.createElement("div") // cria uma nova div
     const img = document.createElement("img") //criar elemento img
     const cardInformation = document.createElement("div") //cria div das informações
-
+    const powerTypeZero = document.createElement("div")
+    const power = document.createElement("div")
     img.src = pokemons.img // coloca o endereço da imagem
 
     //card.classList.add("card") // coloca uma clas na div
-    card.classList = (pokemons.type[0])
+    powerTypeZero.classList = (pokemons.type[0])
     card.classList.add("card")
+    powerTypeZero.classList.add("power")
     img.classList.add("picture") // coloca uma classe na imagem
     cardInformation.classList.add("card-information")
     cardInformation.innerHTML += `<br><h5>${pokemons.num}</h5>` //coloca o numero
     cardInformation.innerHTML += `<h3>${pokemons.name}</h4>` // coloca o nameInput
-    cardInformation.innerHTML += `<h5>${pokemons.type}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
+    powerTypeZero.innerHTML += `<h5>${pokemons.type[0]}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
+    
+    power.appendChild(powerTypeZero)
+    if(pokemons.type[1] != undefined)
+    {
+        const powerTypeOne = document.createElement("div")
+        powerTypeOne.classList = (pokemons.type[1])
+        powerTypeOne.classList.add("power")
+        powerTypeOne.innerHTML += `<h5>${pokemons.type[1]}</h5>`
+        power.appendChild(powerTypeOne)
+    }
+    
+    cardInformation.appendChild(power)
     card.appendChild(cardInformation) // coloca informações no card
     card.appendChild(img) //coloca a imagem na nova div
     root.appendChild(card) // coloca nova div dentro da div existente
@@ -44,14 +58,14 @@ const main = () => {
     const elementMain = pokemons.filter(funcMain)
     elementMain.map(print)
     const viewAll = creatButtonView()
-    viewAll.innerHTML = "Ver Tudo"
+    viewAll.innerHTML = "<h3>Ver Tudo</h3>"
     viewAll.addEventListener('click', viewAllElement)
 }
 const viewAllElement = () => {
     clearDisplay()
     pokemons.map(print)
     const viewAll = creatButtonView()
-    viewAll.innerHTML = "Ver Menos"
+    viewAll.innerHTML = "<h3>Ver Menos</h3>"
     viewAll.addEventListener('click', main)
 }
 
