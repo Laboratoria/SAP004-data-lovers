2ebae7d11c229ac3017044fd4b89f9cb668bfe2a
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-28 02:19:37 -0300

    pokemons com cards coloridos de acordo com o tipo

--- src/main.js
+++ src/main.js
@@ -6,18 +6,19 @@ const pokemons = data.pokemon // pokemons = array
 
 const print = pokemons => { //função para imprimir os pokemons
     const card = document.createElement("div") // cria uma nova div
-    const img = document.createElement("img"); //criar elemento img
+    const img = document.createElement("img") //criar elemento img
     const cardInformation = document.createElement("div") //cria div das informações
 
     img.src = pokemons.img // coloca o endereço da imagem
 
-    card.classList.add("card") // coloca uma clas na div
+    //card.classList.add("card") // coloca uma clas na div
+    card.classList = (pokemons.type[0])
+    card.classList.add("card")
     img.classList.add("picture") // coloca uma classe na imagem
     cardInformation.classList.add("card-information")
     cardInformation.innerHTML += `<br><h5>${pokemons.num}</h5>` //coloca o numero
     cardInformation.innerHTML += `<h3>${pokemons.name}</h4>` // coloca o nameInput
-    cardInformation.innerHTML += `<h5>${pokemons.type[0]} ${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
-
+    cardInformation.innerHTML += `<h5>${pokemons.type}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
     card.appendChild(cardInformation) // coloca informações no card
     card.appendChild(img) //coloca a imagem na nova div
     root.appendChild(card) // coloca nova div dentro da div existente
@@ -30,28 +31,28 @@ const creatButtonView = () => {
 
     card.classList.add("view-all-class")
     card.classList.add("card") // coloca uma clas na div
-    
+
     card.appendChild(viewAll)
     root.appendChild(card) // coloca nova div dentro da div existente
 
     return viewAll
 }
 
-const main = () => {   
+const main = () => {
     clearDisplay()
     const funcMain = (pokemons) => Number(pokemons.num) < 8
     const elementMain = pokemons.filter(funcMain)
     elementMain.map(print)
     const viewAll = creatButtonView()
     viewAll.innerHTML = "Ver Tudo"
-    viewAll.addEventListener('click',viewAllElement)
+    viewAll.addEventListener('click', viewAllElement)
 }
 const viewAllElement = () => {
-     clearDisplay()
-     pokemons.map(print)
+    clearDisplay()
+    pokemons.map(print)
     const viewAll = creatButtonView()
     viewAll.innerHTML = "Ver Menos"
-     viewAll.addEventListener('click',main)
+    viewAll.addEventListener('click', main)
 }
 
 const searchName = () => {
@@ -61,6 +62,5 @@ const searchName = () => {
 }
 
 document.getElementById("search").addEventListener('input', searchName)
-
 
 main()
