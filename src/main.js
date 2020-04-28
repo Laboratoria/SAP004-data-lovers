5758f2c9e7f82bf28e3a91383ae20406918b5b53
Thalita Gonçalves <thagsousa15@gmail.com>
2020-04-28 14:30:22 -0300

    Atualizações

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+function showData() {
+    let firstELement = document.getElementById("listening");
+    let tela = document.createElement("div");
+    for (let i = 0; i <= data.results; i++) {
+        tela.classList.add("card-container");
+        tela.innerHTML = `<img src=${data.results.image[i]}>
+        <h2>Nome:</h2>
+        <p>Gênero</p>`
+    }
+    firstELement.appendChild(tela);
+    return tela;
+}
+
+showData();
