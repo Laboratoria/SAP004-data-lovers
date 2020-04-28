68844b64261f570c15bf6bd724cedfbcd887f3b1
coderanac <coder.anac@gmail.com>
2020-04-28 14:59:06 -0300

    :recycle: refactor the list

--- src/scripts/createCard.js
+++ src/scripts/createCard.js
@@ -1,26 +1,30 @@
 import datum from '../data/rickandmorty/rickandmorty.js';
 
 const createCharacter = (character) => `
-<li class="cards-box">
-  <img src="${character.image}"/>
-  <p>${character.name}</p>
-  <p>${character.species}</p>
-  <p>${character.status}</p>
-  <p>${character.gender}</p>
-  <p>${character.type}</p>
-  <p>${character.origin.name}</p>
-  <p>${character.location.name}</p>
-  <p>${character.episode.length}</p>
+<li class="cards-list">
+  <figure class="front-card">
+    <img src="${character.image}"/>
+    <figcaption>${character.name}</figcaption>
+  </figure>
+  <section class="back-card">
+    <p>${character.species}</p>
+    <p>${character.status}</p>
+    <p>${character.gender}</p>
+    <p>${character.type}</p>
+    <p>${character.origin.name}</p>
+    <p>${character.location.name}</p>
+    <p>${character.episode.length}</p>
+  <section>
 </li>
 `;
 
 function buildHTML() {
-  let cardsList = document.querySelector('#cards-list');
+  let cardsBox = document.querySelector('#cards-box');
   let html = '';
   for (let i of datum.results) {
     html += createCharacter(i);
   }
-  cardsList.innerHTML = html;
+  cardsBox.innerHTML = html;
 }
 
 export default buildHTML;
