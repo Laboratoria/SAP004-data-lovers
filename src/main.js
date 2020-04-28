ae66d2a895b2b8656fb5cba11654d0b5204811a8
camilaroque <milaroque@icloud.com>
2020-04-28 13:53:49 -0300

    personagens tela

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { example } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 //document.getElementById('root').innerHTML = data.results.map((item) => item.name)
-
-
+document.getElementById('root').innerHTML = data.results.map((item) => `
+  <aside>
+    <img src="${item.image}" alt="${item.name}">
+  </aside>
+`)
