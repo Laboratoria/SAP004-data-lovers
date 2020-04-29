b9f1c585f550a68ee216d301d735afdf5c80a6bd
Zaine Nascimento <amandazaine@outlook.com>
2020-04-29 12:35:21 -0300

    Editando HTML

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@
 import data from './data/lol/lol.js';
 
 document.getElementById("show-champions").addEventListener("click", showChampions);
+document.getElementById("about-game").addEventListener("click", showAboutGame);
+document.getElementById("compare-champ-but").addEventListener("click", showCompChan);
+document.getElementById("but-youtube").addEventListener("click", showYoutubers);
 
 function showChampions() {  
+    document.getElementById("characters-div").style.visibility = "visible"; 
+    document.getElementById("characters-div").style.display = "block";
+
+    document.getElementById("lol-too-intro").style.visibility = "hidden"; 
+    document.getElementById("lol-too-intro").style.display = "none"; 
+
+    document.getElementById("about-the-game-div").style.visibility = "hidden"; 
+    document.getElementById("about-the-game-div").style.display = "none"; 
+
+    document.getElementById("compare-champions").style.visibility = "hidden"; 
+    document.getElementById("compare-champions").style.display = "none"; 
+
+    document.getElementById("youtube-media").style.visibility = "hidden"; 
+    document.getElementById("youtube-media").style.display = "none"; 
+
     for (let persona in data.data){ 
     document.getElementById("see-characters-here").innerHTML += `
     <hr>
@@ -16,7 +34,56 @@ function showChampions() {
     };
 };
 
+function showAboutGame(){
+    document.getElementById("characters-div").style.visibility = "hidden"; 
+    document.getElementById("characters-div").style.display = "none";
 
+    document.getElementById("lol-too-intro").style.visibility = "hidden"; 
+    document.getElementById("lol-too-intro").style.display = "none"; 
+
+    document.getElementById("about-the-game-div").style.visibility = "visible"; 
+    document.getElementById("about-the-game-div").style.display = "block"; 
+
+    document.getElementById("compare-champions").style.visibility = "hidden"; 
+    document.getElementById("compare-champions").style.display = "none"; 
+
+    document.getElementById("youtube-media").style.visibility = "hidden"; 
+    document.getElementById("youtube-media").style.display = "none";
+};
+
+function showCompChan(){
+    document.getElementById("characters-div").style.visibility = "hidden"; 
+    document.getElementById("characters-div").style.display = "none";
+
+    document.getElementById("lol-too-intro").style.visibility = "hidden"; 
+    document.getElementById("lol-too-intro").style.display = "none"; 
+
+    document.getElementById("about-the-game-div").style.visibility = "hidden"; 
+    document.getElementById("about-the-game-div").style.display = "none"; 
+
+    document.getElementById("compare-champions").style.visibility = "visible"; 
+    document.getElementById("compare-champions").style.display = "block"; 
+
+    document.getElementById("youtube-media").style.visibility = "hidden"; 
+    document.getElementById("youtube-media").style.display = "none";
+};
+
+function showYoutubers(){
+    document.getElementById("characters-div").style.visibility = "hidden"; 
+    document.getElementById("characters-div").style.display = "none";
+
+    document.getElementById("lol-too-intro").style.visibility = "hidden"; 
+    document.getElementById("lol-too-intro").style.display = "none"; 
+
+    document.getElementById("about-the-game-div").style.visibility = "hidden"; 
+    document.getElementById("about-the-game-div").style.display = "none"; 
+
+    document.getElementById("compare-champions").style.visibility = "hidden"; 
+    document.getElementById("compare-champions").style.display = "none"; 
+
+    document.getElementById("youtube-media").style.visibility = "visible"; 
+    document.getElementById("youtube-media").style.display = "block";
+};
 
 
      
