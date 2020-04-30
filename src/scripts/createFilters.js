57619ec600425ba87eeda9822d94283fcb20cbcf
coderanac <coder.anac@gmail.com>
2020-04-30 10:31:40 -0300

    :art: create filters

---
+++ src/scripts/createFilters.js
@@ -0,0 +1,21 @@
+export function getOptions(characters, prop) {
+  return characters.reduce(
+    (acc, character) => acc.add(character[prop]),
+    new Set(),
+  );
+}
+
+export function createFilter(parentElement, options, callback) {
+  const list = [];
+
+  options.forEach((element) => {
+    const li = document.createElement('li');
+    li.addEventListener('click',
+      () => callback(element)
+    );
+    li.innerHTML = element;
+    list.push(li);
+  })
+
+  parentElement.append(...list);
+}
