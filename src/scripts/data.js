23af92cfbaff37dd35948d70b2fabe29a1f039b7
coderanac <coder.anac@gmail.com>
2020-04-30 10:32:11 -0300

    :poop: refactor filters

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,10 +1,3 @@
-export function filterData (data, key, value) {
-  function filter(item) {
-    //item atual que esta dos dados na propriedade da chave(exemplo gender) igual o valor(exemplo male)//
-    return item[key] === value
-  }
-  return data.filter(filter)
-}
 export function sortData (data, by, order) {
   function sortAsc(a, b) {
     if (a[by] > b[by]){
@@ -26,7 +19,7 @@ export function sortData (data, by, order) {
     return 0;
   }
 
-  
+
   if(order === 'desc') {
     return data.sort(sortDesc)
   }
