2b139dfc94b74872970fd9b013fc489d87f7480b
Thalita Gonçalves <thagsousa15@gmail.com>
2020-04-30 12:35:51 -0300

    getting data

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
-function showData() {
-    let firstELement = document.getElementById("listening");
-    let tela = document.createElement("div");
-    for (let i = 0; i <= data.results; i++) {
-        tela.classList.add("card-container");
-        tela.innerHTML = `<img src=${data.results.image[i]}>
-        <h2>Nome:</h2>
-        <p>Gênero</p>`
-    }
-    firstELement.appendChild(tela);
-    return tela;
-}
+let newELement = document.createElement("div");
+let myData = document.createTextNode(data.results.map(({name, status, species, image}) => `
+Nome: ${name}
+Status: ${status}
+Espécie: ${species}`))
+let gettingElement = newELement.appendChild(myData);
+document.getElementById("listening").appendChild(gettingElement);
 
-showData();
