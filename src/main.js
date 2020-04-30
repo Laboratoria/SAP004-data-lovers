11c0929cc0fc864d76fac0c5b08272bd49d05574
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-04-30 11:55:12 -0300

    Co-authored-by: Adriana Silva <Adriana.sj@live.com>

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const pokemons = data.pokemon;
+
+const card = (num, name, img) => {
+    return `<figure class="card-pokemon"><p>${num}</p>
+    <img src="${img}" alt="Miniatura do pokémon ${name}">
+    <figcaption>${name}</figcaption>
+    </figure>`
+}
+
+//pokemons.map(pokemon => document.getElementById("root").innerHTML += card(pokemons.num, pokemons.name, pokemons.img);
+
+
+pokemons.map(pokemon => document.getElementById("root").innerHTML += card (pokemons.num, pokemons.name, pokemons.img));
