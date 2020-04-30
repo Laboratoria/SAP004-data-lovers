5971a925306ae8b1702119254858d508d287a7e5
larissamiyaji <natsumi-23@hotmail.com>
2020-04-30 00:14:28 -0300

    Printa os personagens na tela em formato de lista

--- src/main.js
+++ src/main.js
@@ -1,6 +1,96 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+import data from "./data/lol/lol.js";
+import lol from "./data/lol/lol.js";
 
-console.log(example, data);
+const aboutGameButton = document.getElementById("about-game");
+aboutGameButton.addEventListener("click", showAboutGame);
+const showChampionsButton = document.getElementById("show-champions");
+showChampionsButton.addEventListener("click", showChampions);
+const compareChampionButton = document.getElementById("compare-champ-but");
+compareChampionButton.addEventListener("click", showCompChan);
+const youtubeButton = document.getElementById("but-youtube");
+youtubeButton.addEventListener("click", showYoutubers);
+const charactersDiv = document.getElementById("characters-div");
+const lolToolIntro = document.getElementById("lol-too-intro");
+const aboutTheGameDiv = document.getElementById("about-the-game-div");
+const compareChampion = document.getElementById("compare-champions");
+const youtubeMedia = document.getElementById("youtube-media");
+// DECLAREI VARIÁVEIS PARA OS BOTÕES PARA FICAR MAIS FÁCIL DE TRABALHAR, AO INVÉS DE SEMPRE CHAMAR COM getElementeById
+function showChampions() {
+  charactersDiv.style.visibility = "visible";
+  charactersDiv.style.display = "block";
+
+  lolToolIntro.style.visibility = "hidden";
+  lolToolIntro.style.display = "none";
+
+  aboutTheGameDiv.style.visibility = "hidden";
+  aboutTheGameDiv.style.display = "none";
+
+  compareChampion.style.visibility = "hidden";
+  compareChampion.style.display = "none";
+
+  youtubeMedia.style.visibility = "hidden";
+  youtubeMedia.style.display = "none";
+
+  for (let persona in data.data) {
+    document.getElementById("see-characters-here").innerHTML += `
+    <hr>
+    <img src="${data.data[persona].img}"> <br>
+    Name: ${persona} <br>    
+    ${data.data[persona].title} <br>
+    Role: ${data.data[persona].tags} <br>
+    <hr>
+    `;
+  }
+}
+
+function showAboutGame() {
+  charactersDiv.style.visibility = "hidden";
+  charactersDiv.style.display = "none";
+
+  lolToolIntro.style.visibility = "hidden";
+  lolToolIntro.style.display = "none";
+
+  aboutTheGameDiv.style.visibility = "visible";
+  aboutTheGameDiv.style.display = "block";
+
+  compareChampion.style.visibility = "hidden";
+  compareChampion.style.display = "none";
+
+  youtubeMedia.style.visibility = "hidden";
+  youtubeMedia.style.display = "none";
+}
+
+function showCompChan() {
+  charactersDiv.style.visibility = "hidden";
+  charactersDiv.style.display = "none";
+
+  lolToolIntro.style.visibility = "hidden";
+  lolToolIntro.style.display = "none";
+
+  aboutTheGameDiv.style.visibility = "hidden";
+  aboutTheGameDiv.style.display = "none";
+
+  compareChampion.style.visibility = "visible";
+  compareChampion.style.display = "block";
+
+  youtubeMedia.style.visibility = "hidden";
+  youtubeMedia.style.display = "none";
+}
+
+function showYoutubers() {
+  charactersDiv.style.visibility = "hidden";
+  charactersDiv.style.display = "none";
+
+  lolToolIntro.style.visibility = "hidden";
+  lolToolIntro.style.display = "none";
+
+  aboutTheGameDiv.style.visibility = "hidden";
+  aboutTheGameDiv.style.display = "none";
+
+  compareChampion.style.visibility = "hidden";
+  compareChampion.style.display = "none";
+
+  youtubeMedia.style.visibility = "visible";
+  youtubeMedia.style.display = "block";
+}
