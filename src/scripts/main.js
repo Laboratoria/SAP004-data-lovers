745940a931a4cda26093a40323b4fdec3c2963a4
coderanac <coder.anac@gmail.com>
2020-04-30 14:16:40 -0300

    :art: Create search

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,15 @@
 import buildHTML from './createCard.js'
 import data from '../data/rickandmorty/rickandmorty.js';
-import { allCharacters, gendersFilter, speciesFilter, sortFilter, statusFilter } from './selectors.js'
+import { allCharacters, gendersFilter, search, speciesFilter, sortFilter, statusFilter } from './selectors.js'
 import { createFilter, getOptions } from './createFilters.js';
 
-
 allCharacters.addEventListener('click', () => buildHTML(data.results))
+
+search.addEventListener('input', () => {
+  let name = search.value;
+  let listCharacters = data.results.filter((character) => character.name.toLowerCase().includes(name.toLowerCase()));
+  buildHTML(listCharacters);
+})
 
 function buildFilter(parentElement, type) {
   const options = getOptions(data.results, type);
--- src/scripts/selectors.js
+++ src/scripts/selectors.js
@@ -2,8 +2,9 @@ const allCharacters = document.querySelector('#allCharacters');
 const statusFilter = document.querySelector('#statusFilter');
 const cardsBox = document.querySelector('#cards-box');
 const gendersFilter = document.querySelector('#genderFilter');
+const search = document.querySelector('#search');
 const speciesFilter = document.querySelector('#speciesFilter');
 const sortFilter = document.querySelector('#sortFilter');
 
 
-export { allCharacters, cardsBox, gendersFilter, speciesFilter, sortFilter, statusFilter };
+export { allCharacters, cardsBox, gendersFilter, search, speciesFilter, sortFilter, statusFilter };
