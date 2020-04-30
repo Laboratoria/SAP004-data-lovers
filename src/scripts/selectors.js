26c40db1b5fdc1dae3cb60e5ab8a62d380783900
coderanac <coder.anac@gmail.com>
2020-04-30 10:30:29 -0300

    :art: create selectors

---
+++ src/scripts/selectors.js
@@ -0,0 +1,9 @@
+const allCharacters = document.querySelector('#allCharacters');
+const statusFilter = document.querySelector('#statusFilter');
+const cardsBox = document.querySelector('#cards-box');
+const gendersFilter = document.querySelector('#genderFilter');
+const speciesFilter = document.querySelector('#speciesFilter');
+const sortFilter = document.querySelector('#sortFilter');
+
+
+export { allCharacters, cardsBox, gendersFilter, speciesFilter, sortFilter, statusFilter };
