0effa6f19223367ab3940a1e3844bca007dbe1c9
coderanac <coder.anac@gmail.com>
2020-05-05 15:37:04 -0300

    :zap: improves buildHTML

--- src/scripts/createCard.js
+++ src/scripts/createCard.js
@@ -1,9 +1,7 @@
-import { cardsBox } from './selectors.js';
-
-const createCharacter = (character) => `
+export const createCharacter = (character) => `
 <li class="card__character">
   <figure>
-    <img src="${character.image}" class="card__image"/>
+    <img src="${character.image}" class="card__image">
     <figcaption class="card__name">${character.name}</figcaption>
   </figure>
   <section class="card__description">
@@ -14,16 +12,14 @@ const createCharacter = (character) => `
     <p><span>Origin:</span> ${character.origin.name}</p>
     <p><span>Location:</span> ${character.location.name}</p>
     <p><span>Episodes:</span> ${character.episode.length}</p>
-  <section>
+  </section>
 </li>
 `;
 
-function buildHTML(dataCharacter) {
+export function buildHTML(parentElement, dataCharacter) {
   let html = '';
   for (let i of dataCharacter) {
     html += createCharacter(i);
   }
-  cardsBox.innerHTML = html;
+  parentElement.innerHTML = html;
 }
-
-export default buildHTML;
