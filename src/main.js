a4ef449d4c70f4a0e452f8f82d68382a8170260a
sjadriana <adriana.sj@live.com>
2020-05-05 00:01:04 -0300

    Atualização de funções 1

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //import { searchFunc } from './data.js';
-const pokemons = data.pokemon;
+const pokemons = data.pokemon; 
+
+function compare (a, b) {
+ console.log( a.name - b.name)
+}
 const searchBar = document.querySelector('#searchBar');
-const btnSrc = document.querySelector("#btn-src")
+const btnorder = document.querySelector("#btn-od")
 
 //apenas mostra personagens na tela
 /*const card = (num, name, img,type) => {
@@ -44,12 +48,39 @@ searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
     const filteredCharacters = pokemons.filter((character) => {
         return (
-            character.name.toLowerCase().includes(searchString) //|| character.type.toLowerCase().includes(searchStringS)           
+            character.name.toLowerCase().includes(searchString) //|| character.type.toLowerCase().includes(searchStringS)
+                   
         );
-    });
-    displayCharacters(filteredCharacters);
-});
+    }); 
+    displayCharacters(filteredCharacters);  
+}); 
 
+//função a-z 
+/*btnorder.addEventListener("click", () => {
+    const sortedA = pokemons.sort( (a, b) => {
+    if( a.name > b.name) {return 1}
+    else if(b.name > a.name) { -1}
+    else { 0};})
+} );
+//console.log( sortedA );
+
+/*função z-a
+const sortedZ = pokemons.sort( (a, b) => {
+    if( b.name > a.name) return 1;
+    else if(a.name > b.name) return -1;
+    else return 0;
+} );
+//console.log( sortedZ );
+
+// função id
+const sortedId = pokemons.sort((a, b)=> {
+    if (b.id > a.id) return 1;
+    else if (a.id > b.id) return -1;
+    else return 0
+});console.log( sortedId);
+
+*/
+//lidando com erros
 const loadCharacters = async () => {
     try {
        
@@ -59,7 +90,7 @@ const loadCharacters = async () => {
         console.error(err);
     }
 };
-
+//mostrar pokemons na tela
 const displayCharacters = (characters) => {
     const htmlString = characters
         .map((character) => {
