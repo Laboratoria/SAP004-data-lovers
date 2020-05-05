06708743cbce00da8fa8ffacc7def4c0b33463ee
coderanac <coder.anac@gmail.com>
2020-05-05 15:39:28 -0300

    :art: create test creates the test for the data

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,31 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterExact, filterLike, sortByProp } from '../src/scripts/data.js';
 
+import { expect } from 'chai';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const character = [
+  { name: "Morty" },
+  { name: "Rick" }
+];
+
+describe('filter exact', () => {
+  it('filter exact', () => {
+    expect(filterExact(character, 'name', 'Morty')).to.deep.equal([character[0]]);
   });
+});
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe('filter like', () => {
+  it('filter like', () => {
+    expect(filterLike(character, 'name', 'mor')).to.deep.equal([character[0]]);
+  });
+});
+
+describe('sort', () => {
+  it('a-z', () => {
+    expect(sortByProp(character, 'name', false)).to.deep.equal([{"name": "Rick"}, {"name": "Morty"}]);
+  });
+  it('z-a', () => {
+    expect(sortByProp(character, 'name', true)).to.deep.equal([{"name": "Morty"}, {"name": "Rick"}]);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
