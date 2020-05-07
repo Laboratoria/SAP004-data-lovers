dac241a30be1c6d9c9d81df867966b4386cd22cd
coderanac <coder.anac@gmail.com>
2020-05-07 14:02:13 -0300

    :recycle: refactor main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+import { getCharacters } from '../data/rickandmorty.js'
 import { buildHTML } from './createCard.js'
 import { allCharacters, gendersFilter, search, speciesFilter, sortByNameUp, sortByNameDown, statusFilter, cardsBox } from './selectors.js'
 import { createFilter, getOptions } from './createFilters.js';
@@ -20,25 +21,6 @@ function sortCharacter(data, prop, upOrDown) {
 function searchCharacter(data) {
   let listCharacters = filterLike(data, 'name', event.target.value);
   buildHTML(cardsBox, listCharacters);
-}
-
-async function getCharacters() {
-  const characters = [];
-  const promises = [];
-  const getPage = (i) => fetch(`https://rickandmortyapi.com/api/character/?page=${i}`).then(response => response.json());
-
-  const firstPage = await getPage(1);
-
-  for (let i = 2; i < firstPage.info.pages; i++) {
-    promises.push(getPage(i));
-  }
-
-  characters.push(...firstPage.results);
-
-  // eslint-disable-next-line no-undef
-  (await Promise.all(promises)).forEach((responses) => characters.push(...responses.results));
-
-  return characters;
 }
 
 getCharacters().then(characters => {
