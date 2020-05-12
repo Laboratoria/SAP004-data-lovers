5ee542b4cd0f56390edffb1d085def7c8369c770
sjadriana <adriana.sj@live.com>
2020-05-12 19:29:31 -0300

    alteração

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,52 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterByType, sortOrder } from '../src/data.js';
 
+const pokemonMock = [{
+  id: 1,
+  name: "Bulbasaur",
+  type: ["Grass", "Poison"],
+  },
+  {
+  id: 4,
+  name: "Charmander",
+  type: ["Fire"],
+  }
+];
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('Deve filtrar o pokémon por tipo', () => {
+  it('é uma função', () => {
+    expect(typeof filterByType).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('retorna o objeto do pokémon quando filtrado por tipo', () => {
+    expect(filterByType("Grass", "type", pokemonMock)).toEqual([{"id": 1, "name": "Bulbasaur", "type": ["Grass", "Poison"]}])
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('Deve ordernar os pokémons de A-Z e Z-A ou 1-151 a 151-1', () => {
+  it('é uma função', () => {
+    expect(typeof sortOrder).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('retorna a lista de pokémon impresso na tela quando ordenado a-z', () => {
+    expect(typeof sortOrder("az", "name", pokemonMock)).toBeTruthy();
+  });
+
+  it('retorna a lista de pokémon impresso na tela quando ordenado z-a', () => {
+    expect(typeof sortOrder("za", "name", pokemonMock)).toBeTtruthy();
   });
 });
+
+
+
+/*
+describe('Deve buscar os pokémons pelo nome', () => {
+  it('é uma função', () => {
+    expect(typeof searchPokemon).toBe('function');
+  });
+  
+});
+sortOrder, searchPokemon
+*/
+
+
