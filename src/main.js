d0ab8b136b3f5834cdd0ad1d35bf4a6706f83681
Gisele Cesar <gisele-cesar@hotmail.com>
2020-05-12 13:36:22 -0300

    Finalização do CSS para desktop contendo arquivos style.css e main.js

--- src/main.js
+++ src/main.js
@@ -11,48 +11,11 @@ function allList(list) {
 
         let cardItem = document.createElement("div"); //criando pequenos cards dentro da div maior do html
         cardItem.className = "div-child"; //criei essa cGlasse apenas para teste no css
-        
-        let imgItem = document.createElement("img"); //criando div imagem dentro da div peq card
-        imgItem.src = obj.img;  //setando qual valor do objeto aparecerá na div imagem
-        imgItem.className = "img-item";
-        cardItem.appendChild(imgItem); //colocando a div de img dentro da pequena div
 
-        let spaceTextCard = document.createElement("div");
-        
-        spaceTextCard.innerHTML += `<h5>${obj.num}</h5>
+        cardItem.innerHTML += `<img src = ${obj.img} class = "img-item"><h5>${obj.num}</h5>
         <h2>${obj.name.replace("(Female)", "").replace("(Male)", "")}</h2>
         <p>Tipo: ${obj.type.join(", ")}</p><p>Ovo: ${obj.egg}</p>
-        <p>Fraquezas:</p><p>${obj.weaknesses.join(", ")}</p>`;
-               
-        spaceTextCard.className = "div-inside"
-        cardItem.appendChild(spaceTextCard);
-
-       /* let numberItem = document.createElement("h5");
-        numberItem.className = "text-card";
-        numberItem.innerHTML = obj.num;
-        cardItem.appendChild(numberItem);
-
-        let nameItem = document.createElement("h2"); //criando div para colocar o nome do pokemon
-        nameItem.className = "text-card";
-        nameItem.textContent = obj.name; //indicando qual valor do objeto é o nome
-        cardItem.appendChild(nameItem); //colocando a div nome dentro do pequeno card
-
-        let typeItem = document.createElement("p");
-        typeItem.className = "text-card";
-        typeItem.textContent = `Tipo: ${obj.type}`;
-        cardItem.appendChild(typeItem);
-
-        let eggItem = document.createElement("p");
-        eggItem.className = "text-card";
-        eggItem.textContent = `Ovo: ${obj.egg}`;
-        cardItem.appendChild(eggItem);
-
-        let weaknessesItem = document.createElement("p");
-
-        weaknessesItem.className = "text-card";
-        //weaknessesItem.textContent = `Fraquezas: ${obj.weaknesses}`;  
-        weaknessesItem.textContent = obj.weaknesses.join(", ");
-        cardItem.appendChild(weaknessesItem);*/
+        <h3>Fraquezas:</h3><p>${obj.weaknesses.join(", ")}</p>`;  
 
         cards.appendChild(cardItem);
     }
