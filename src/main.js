9cd80df990217e18e00d58fb19812289502ff3ad
sjadriana <adriana.sj@live.com>
2020-05-17 18:10:03 -0300

    virgula entre tipos

--- src/main.js
+++ src/main.js
@@ -49,9 +49,49 @@ const buttonFlying = document.querySelector("#buttonFlying");
 // card do pokémon
 const card = (num, name, img, type) => {
 
+  let icon = type.map(function(name){
+    if (name === "Stell"){
+      return name.replace("Stell", `<span class="icon-aco stell"></span>`);
+    } else if (name === "Water"){
+      return name.replace("Water", `<span class="icon-agua water"></span>`);
+    } else if (name === "Dragon"){
+      return name.replace("Dragon", `<span class="icon-dragao dragon"></span>`);
+    } else if (name === "Electric"){
+      return name.replace("Electric", `<span class="icon-eletrico eletric"></span>`);
+    } else if (name === "Fairy"){
+      return name.replace("Fairy", `<span class="icon-fada fairy"></span>`);
+    } else if (name === "Ghost"){
+      return name.replace("Ghost", `<span class="icon-fantasma ghost"></span>`);
+    } else if (name === "Fire"){
+      return name.replace("Fire", `<span class="icon-fogo fire"></span>`);
+    } else if (name === "Ice"){
+      return name.replace("Ice", `<span class="icon-fada ice"></span>`);
+    } else if (name === "Bug"){
+      return name.replace("Bug", `<span class="icon-inseto bug"></span>`);
+    } else if (name === "Fighting"){
+      return name.replace("Fighting", `<span class="icon-lutador fighting"></span>`);
+    } else if (name === "Normal"){
+      return name.replace("Normal", `<span class="icon-normal normal"></span>`);
+    } else if (name === "Dark"){
+      return name.replace("Dark", `<span class="icon-noturno dark"></span>`);
+    } else if (name === "Rock"){
+      return name.replace("Rock", `<span class="icon-pedra rock"></span>`);
+    } else if (name === "Grass"){
+      return name.replace("Grass", `<span class="icon-planta grass"></span>`);
+    } else if (name === "Psychic"){
+      return name.replace("Psychic", `<span class="icon-psiquico psychic"></span>`);
+    } else if (name === "Ground"){
+      return name.replace("Ground", `<span class="icon-terrestre ground"></span>`);
+    } else if (name === "Poison"){
+      return name.replace("Poison", `<span class="icon-venenoso poison"></span>`);
+    } else if (name === "Flying"){
+      return name.replace("Flying", `<span class="icon-voador flying"></span>`);
+    }
+  }).join('')
+  
   return `<figure class="card-pokemon"><p class="pokemon-num">${num}</p>
   <img src="${img}" alt="Miniatura do pokémon ${name}">
-  <p class="pokemon-type">${type}</p>
+  <p class="pokemon-type">${icon}</p>
   <figcaption class="pokemon-name">${name}</figcaption>
   </figure>`
 };
@@ -77,9 +117,50 @@ searchBar.addEventListener("keyup",() => displayPokemons(searchPokemon(searchBar
 const displayPokemons = (pokemon) => {
   const cards = pokemon
     .map((pokemon) => {
+
+      let type = pokemon.type.map(function(name){
+        if (name === "Stell"){
+          return name.replace("Stell", `<span class="icon-aco stell"></span>`);
+        } else if (name === "Water"){
+          return name.replace("Water", `<span class="icon-agua water"></span>`);
+        } else if (name === "Dragon"){
+          return name.replace("Dragon", `<span class="icon-dragao dragon"></span>`);
+        } else if (name === "Electric"){
+          return name.replace("Electric", `<span class="icon-eletrico eletric"></span>`);
+        } else if (name === "Fairy"){
+          return name.replace("Fairy", `<span class="icon-fada fairy"></span>`);
+        } else if (name === "Ghost"){
+          return name.replace("Ghost", `<span class="icon-fantasma ghost"></span>`);
+        } else if (name === "Fire"){
+          return name.replace("Fire", `<span class="icon-fogo fire"></span>`);
+        } else if (name === "Ice"){
+          return name.replace("Ice", `<span class="icon-fada ice"></span>`);
+        } else if (name === "Bug"){
+          return name.replace("Bug", `<span class="icon-inseto bug"></span>`);
+        } else if (name === "Fighting"){
+          return name.replace("Fighting", `<span class="icon-lutador fighting"></span>`);
+        } else if (name === "Normal"){
+          return name.replace("Normal", `<span class="icon-normal normal"></span>`);
+        } else if (name === "Dark"){
+          return name.replace("Dark", `<span class="icon-noturno dark"></span>`);
+        } else if (name === "Rock"){
+          return name.replace("Rock", `<span class="icon-pedra rock"></span>`);
+        } else if (name === "Grass"){
+          return name.replace("Grass", `<span class="icon-planta grass"></span>`);
+        } else if (name === "Psychic"){
+          return name.replace("Psychic", `<span class="icon-psiquico psychic"></span>`);
+        } else if (name === "Ground"){
+          return name.replace("Ground", `<span class="icon-terrestre ground"></span>`);
+        } else if (name === "Poison"){
+          return name.replace("Poison", `<span class="icon-venenoso poison"></span>`);
+        } else if (name === "Flying"){
+          return name.replace("Flying", `<span class="icon-voador flying"></span>`);
+        }
+      }).join('')
+      
       return `<figure class="card-pokemon"><p class="pokemon-num">${pokemon.num}</p>
       <img src="${pokemon.img}" alt="Miniatura do pokémon ${pokemon.name}">
-      <p class="pokemon-type">${pokemon.type}</p>
+      <p class="pokemon-type">${type}</p>
       <figcaption class="pokemon-name">${pokemon.name}</figcaption>
       </figure>`;
     })
@@ -105,16 +186,9 @@ buttonDecre.addEventListener("click", () => displayPokemons(sortOrder("decre", "
 
 
 // **** Botões Tipo *******
-/*buttonNormal.addEventListener("click", function (){
-  
-  let kanto = document.querySelector("#calc")
-  const numpok = (filterByType.length / 151)*100
-  kanto.innerHTML= `Em <strong>Kanto</strong> há ${numpok}% deste tipo de Pokemon`
-  return pokemonData
-})*/
 
 buttonSteel.addEventListener("click", () => displayPokemons(filterByType("Steel", "type", pokemonData)));
-buttonWater.addEventListener("click", () => displayPokemons(filterByType("Water", "type", pokemonData)) ); 
+buttonWater.addEventListener("click", () => displayPokemons(filterByType("Water", "type", pokemonData)));
 buttonDragon.addEventListener("click", () => displayPokemons(filterByType("Dragon", "type", pokemonData)));
 buttonEletric.addEventListener("click", () => displayPokemons(filterByType("Electric", "type", pokemonData)));
 buttonFairy.addEventListener("click", () => displayPokemons(filterByType("Fairy", "type", pokemonData)));
