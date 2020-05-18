0b1f3e5d04fc2f21b1ab0aa3f8240c4694e0dbf6
renataalmeids <ras_almeida@hotmail.com>
2020-05-18 16:39:50 -0300

    alteração de estrutura

--- src/data.js
+++ src/data.js
@@ -7,11 +7,7 @@ export const applyDescendingOrder = (pokemonGo) => pokemonGo.sort((a, b) =>
 export const applySearchType = (pokemonGo, filterType) => pokemonGo.filter((search) =>
   (search.type.includes(filterType)));
 
-<<<<<<< HEAD
 export const resultCalcType = (pokemonGo, filterType) => 
-=======
-export const resultCalcType = (pokemonGo, filterType) =>
->>>>>>> 45286cac1febcc38558771890678311c85213da0
   ((pokemonGo.length) / 100) * (filterType.length).toFixed(2);
 
 export const applySearchName = (pokemonGo, filterName) => pokemonGo.filter((search) =>
