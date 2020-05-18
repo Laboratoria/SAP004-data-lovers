8340183232003310b89cdc8dd3ee68512dbfc519
Gisele Cesar <gisele-cesar@hotmail.com>
2020-05-18 11:43:37 -0300

    dados estatísticos contendo main.js e data.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { apllyAscendingOrder, applyDescendingOrder, applySearchType, applyCalcType, applySearchName } from './data.js';
+import { apllyAscendingOrder, applyDescendingOrder, applySearchType, resultCalcType, applySearchName  } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
@@ -25,45 +25,26 @@ allList(pokemonGo);
 document.getElementById("A-Z").onclick = () => {
     const orderResult = apllyAscendingOrder(pokemonGo)
     allList(orderResult)
-    document.getElementById("statistics").innerHTML ="";
+    document.getElementById("statistics").innerHTML = "";
 }
 
 document.getElementById("Z-A").onclick = () => {
     const orderResult = applyDescendingOrder(pokemonGo)
     allList(orderResult);
-    document.getElementById("statistics").innerHTML ="";
+    document.getElementById("statistics").innerHTML = "";
 }
 
 document.getElementById("select-type").onchange = () => {
     const filterType = document.getElementById("select-type").value;
     const listType = (applySearchType(pokemonGo, filterType))
+    const filterCalcType = (resultCalcType(pokemonGo, filterType))
+    document.getElementById("statistics").innerHTML = `<p>Os Pokémons do tipo ${filterCalcType} representam ${filterType} % dos Pokémons da primeira geração.</p>`;
     allList(listType);
-    statistics ()
-} 
-
-function statistics () {
-    const filterType = document.getElementById("select-type").value;
-    const filterCalcType = pokemonGo.filter (search => search.type.includes(filterType))
-    const result = Math.round(((filterCalcType.length * 100) /pokemonGo.length) *100)/100
-    document.getElementById("statistics").innerHTML = `Existem ${result} % dos Pokémons do tipo ${filterType}`;
-} 
-
-/*document.getElementById("select-type").addEventListener ("change", calcType);
-    function calcType() {
-        const infoToCalcType = document.getElementById("select-type").value;
-        const filterToCalcType = pokemonGo.filter(function(search) {
-            return search.type.includes(infoToCalcType);
-        })
-        const tipoSelecionado = infoToCalcType.length;
-        const otherCalc = pokemonGo.length;
-        const resultCalcType = ((tipoSelecionado*100) / otherCalc).toFixed(2);
-        document.getElementById("statistics").innerHTML = `Os Pokémons do tipo ${infoToCalcType} representam ${resultCalcType} % dos Pokémons`;
-    }
-
-    allList(listType, statisticsType);*/
+}
 
 document.getElementById("filter-name").onkeyup = () => {
     const filterName = document.getElementById("filter-name").value;
     const newList = (applySearchName(pokemonGo, filterName))
     allList(newList);
+    document.getElementById("statistics").innerHTML = "";
 }
